fbc022fd35c92b470b83ce39a5ca145a48ba5bcd
hexahigh <97455552+hexahigh@users.noreply.github.com>
2023-11-10 15:50:19 +0100

    update

--- merge.js
+++ merge.js
@@ -3,10 +3,11 @@ const path = require('path');
 
 const directoryPath = path.join(__dirname, 'temp');
 const outputPath = path.join(__dirname, 'lists/ultralist-UB.txt');
+const prependText = `! Title: Boofdev's ultralist\n! Description: A giant filter list that blocks ads, trackers and more\n`;
 
 let lines = new Set();
 
-fs.readdirSync(directoryPath, (err, files) => {
+fs.readdir(directoryPath, (err, files) => {
   if (err) {
     return console.log('Unable to scan directory: ' + err);
   } 
@@ -14,11 +15,16 @@ fs.readdirSync(directoryPath, (err, files) => {
   files.forEach((file) => {
     let data = fs.readFileSync(path.join(directoryPath, file), 'utf8');
     let fileLines = data.split('\n');
-    fileLines.forEach((line) => lines.add(line));
-    console.log(`Merged file: ${file}`)
+    fileLines.forEach((line) => {
+      // Only add the line if it doesn't start with "!"
+      if (!line.startsWith('!')) {
+        lines.add(line);
+      }
+    });
+    console.log(`Merged file: ${file}`);
   });
 
-  let uniqueLines = Array.from(lines).join('\n');
+  let uniqueLines = prependText + Array.from(lines).join('\n'); // Prepend the text
   fs.writeFileSync(outputPath, uniqueLines);
   console.log('All files merged into list');
 });
