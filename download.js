abe304cff4bb7112f63f9472e264ef49a127d2a6
Boof <97455552+hexahigh@users.noreply.github.com>
2023-11-10 09:34:23 +0100

    Create download.js

---
+++ download.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const axios = require('axios');
+const path = require('path');
+
+// Read URLs from a JSON file
+const urls = require('./urls.json');
+
+async function downloadFile(fileUrl, outputLocationPath) {
+  const writer = fs.createWriteStream(outputLocationPath);
+
+  return axios({
+    method: 'get',
+    url: fileUrl,
+    responseType: 'stream',
+  }).then(response => {
+    return new Promise((resolve, reject) => {
+      response.data.pipe(writer);
+      let error = null;
+      writer.on('error', err => {
+        error = err;
+        writer.close();
+        reject(err);
+      });
+      writer.on('close', () => {
+        if (!error) {
+          resolve(true);
+        }
+      });
+    });
+  });
+}
+
+async function downloadAll() {
+  for (let i = 0; i < urls.length; i++) {
+    const url = urls[i];
+    console.log(`Downloading file ${i + 1}`);
+    await downloadFile(url, path.resolve(__dirname, 'lists', `file${i + 1}.txt`));
+    console.log(`Downloaded file ${i + 1}`);
+  }
+  console.log('All files downloaded');
+}
+
+downloadAll();
