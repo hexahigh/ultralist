4fb5e61a7ec770048035d906cf7080cd636150fa
hexahigh <97455552+hexahigh@users.noreply.github.com>
2023-11-11 19:48:41 +0100

    added a sorter for the json file

---
+++ util.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+console.log("Sorting urls.json")
+// Sort urls.json
+fs.readFile('urls.json', 'utf8', function(err, data) {
+    if (err) throw err;
+
+    let array = JSON.parse(data);
+
+    array.sort();
+
+    // Write the sorted array back to the JSON file
+    fs.writeFile('urls.json', JSON.stringify(array, null, 2), 'utf8', function(err) {
+        if (err) throw err;
+        console.log('The file has been saved with the sorted array!');
+    });
+});
+console.log("Sorted urls.json")
